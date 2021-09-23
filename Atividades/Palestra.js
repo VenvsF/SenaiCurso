9ac006aa1d86efa8176831177d99a2736e723091
VenvsF <89944640+VenvsF@users.noreply.github.com>
2021-09-23 16:30:11 -0300

    Update Palestra.js

--- Atividades/Palestra.js
+++ Atividades/Palestra.js
@@ -1,21 +1,21 @@
 'use strict';
-var realinesync = require("readline-sync");
+var readlinesync = require("readline-sync");
 var atendentes = 100;
 var evento = ["Tecnologia", "Quimica", "Natureza"];
 var palestrantes = ["Carlos", "Mariana", "Otavio"];
 const DataEvento = ('2021-10-31');
 let DataAtual = ('2021-9-28');
-let SelecEvento = realinesync.question("Selecione o evento e palestrante: ");
+let SelecEvento = readlinesync.question("Selecione o evento e palestrante: ");
     if(DataEvento<DataAtual) {}
     else{
         console.log("Evento ja ocorreu");
     return};
-let Nome = realinesync.question("Seu nome: ");  
+let Nome = readlinesync.question("Seu nome: ");  
     if(atendentes<100){} 
     else{
         console.log("Evento Lotado"); 
         return};
-let Idade = realinesync.question("Sua idade: ");
+let Idade = readlinesync.question("Sua idade: ");
     if(Idade>18){} 
     else {
         console.log("Evento apenas para maiores de 18.");         
